abbe97250633af8c9f66ef5d700c47066c2ef0f7
dirogo53 <dirogo53@gmail.com>
2021-12-16 20:56:34 -0500

    beautyfront

--- src/home/views/home.view.jsx
+++ src/home/views/home.view.jsx
@@ -4,15 +4,16 @@ import { Link } from 'react-router-dom';
 
 // assets
 import logo from 'assets/logo.svg';
-//import Carousel from 'src/components/carousel.component';
+
 
 const Home = () => {
   return (
   <>
-    
+   
         <img src={logo} className="App-logo" alt="logo"/>
       <p>
        <h1>Proyección-Sistema de Gestión de Proyectos</h1>
+       
       </p>
       
       <Link to="projects">{'Ver los proyectos'}</Link>
